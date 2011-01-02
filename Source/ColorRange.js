2aa10a45db1327501edfcbb04d5a39ffbe23a7e6
Mattijs Ugen <mail@nope.com>
2011-01-02 14:05:23 +0100

    moved mootools files to demo folder, first working version of script

--- Source/ColorRange.js
+++ Source/ColorRange.js
@@ -3,19 +3,19 @@ var ColorRange = new Class({
     Extends: Array,
 
     from: false,
-    to: false,
+    to:   false,
 
     initialize: function(from, to, steps) {
         this.from = new Color(from);
-        this.to = new Color(to);
+        this.to   = new Color(to);
 
-        this.setSteps(stepsi || 0);
+        this.setSteps(steps || 0);
     },
 
     setSteps: function(steps) {
         this.empty();
         for (var index = 0; index < steps; index++) {
-            this[index] = this.from.mix(this.to, (steps - index) * 100);
+            this.push(this.from.mix(this.to, index / (steps - 1) * 100));
         }
 
         return this;
@@ -25,21 +25,33 @@ var ColorRange = new Class({
         if (this.length == elements.length) {
             elements.each(function(element, index) {
                 element.setStyle('background-color', this[index]);
-            });
-            return elements;
+            }, this);
+            
+            return this;
         } else {
             return new ColorRange(this.from, this.to, elements.length).apply(elements);
         }
     }
 
-}
+});
 
 Color.range = function(from, to, steps) {
     return new ColorRange(from, to, steps);
 };
 
+Color.applyRange = function(from, to, elements) {
+    return new ColorRange(from, to, elements.length).apply(elements);
+};
+
 Color.implement({
     range: function(to, steps) {
-        return new ColourRange(this, to, steps);
+        return new ColorRange(this, to, steps);
+    }
+});
+
+Elements.implement({
+    applyColorRange: function(from, to) {
+        new ColorRange(from, to, this.length).apply(this);
+        return this;
     }
 });
